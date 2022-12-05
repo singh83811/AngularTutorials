ed2f3966e3ddfffcbcfc844942f76c5d7f50066c
Bhupender Singh <singh83811@gmail.com>
2022-12-05 18:49:41 +0530

    learn angular 13 new concepts

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angularTutorial';
+  // data = 'Make Angular Application'; // set dynamic variable value
+
+  // getValue(){
+  //   return "get function value"; //set function value
+  // }
+  // getName(name: any) {
+  //   alert(name)
+  // }
+  // getData(box: any) {
+  //   console.log(box)
+  // }
+  // displayVal = '';
+  // getValue(val: string) {
+  //   console.log(val);
+  //   this.displayVal = val
+  // }
+  // count = 0;
+  // counter(type: any) {
+  //   type === 'add' ? this.count++ : this.count--;
+  // }
+
+  // name = 'peter'
+  // disable = false;
+
+  // show = false;
+  // color = "red"
+
+  // color = 'green';
+
+
+}
